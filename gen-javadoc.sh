681388052d63b89b4fa545beea62679ceec8f24f
Bruno Freudensprung <bruno.freudensprung@kairntech.com>
2022-08-31 17:13:31 +0200

    non verbose

--- gen-javadoc.sh
+++ gen-javadoc.sh
@@ -2,7 +2,6 @@
 javadoc -link https://javadoc.io/doc/io.vertx/vertx-core/latest/ \
   -link https://mongodb.github.io/mongo-java-driver/3.6/javadoc/ \
   -link https://mongodb.github.io/mongo-java-driver-reactivestreams/1.13/javadoc \
-  -verbose \
   -classpath /home/bruno/.sdkman/candidates/java/8.0.302-open/lib/tools.jar:target/dependencies/mongodb-driver-core-4.1.2.jar:target/dependencies/mongodb-driver-reactivestreams-4.1.2.jar:target/dependencies/vertx-core-4.3.2.jar:target/dependencies/vertx-codegen-4.3.2.jar:target/dependencies/bson-4.1.2.jar:target/dependencies/reactive-streams-1.0.2.jar:target/dependencies/netty-buffer-4.1.78.Final.jar:target/classes -sourcepath src/main/java:src/main/generated-java \
   -d javadoc \
   -public \
